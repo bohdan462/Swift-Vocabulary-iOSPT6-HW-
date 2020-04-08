a124a137acc6762ec75a2c245e979709550b952e
bohdan462 <tech.bohdan@gmail.com>
2020-04-08 13:54:50 -0500

    New project created

---
+++ Swift Vocabulary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Bohdan Tkachenko on 4/8/20.
+//  Copyright © 2020 Bohdan Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
