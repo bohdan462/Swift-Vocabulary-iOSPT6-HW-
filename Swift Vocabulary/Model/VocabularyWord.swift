7c1300485b55dc0f6619e9e1d7a88d3fd13f2cba
bohdan462 <tech.bohdan@gmail.com>
2020-04-08 14:56:31 -0500

    Added Controllers and created MVC

---
+++ Swift Vocabulary/Model/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Bohdan Tkachenko on 4/8/20.
+//  Copyright © 2020 Bohdan Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+struct VocabularyWord {
+    var word: String
+    var definition: String
+}
---
+++ Swift Vocabulary/View Controller/DefinitionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DefinitionViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Bohdan Tkachenko on 4/8/20.
+//  Copyright © 2020 Bohdan Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    
+    
+    @IBOutlet weak var labelName: UILabel!
+    
+    @IBOutlet weak var textField: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
